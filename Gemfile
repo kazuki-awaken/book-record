4496ef05116d4f132f14a56ff56cae42e0ff8f4f
kazuki suzuki <macchaner@gmail.com>
2021-01-27 15:06:53 +0900

    cloudinary setting gemfile uplaoder

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari'
 
 gem 'carrierwave'
+gem 'cloudinary'
 
 gem 'mini_magick'
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  include Cloudinary::CarrierWave
+
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+    CarrierWave.configure do |config|
+      config.cache_storage = :file
+    end
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
